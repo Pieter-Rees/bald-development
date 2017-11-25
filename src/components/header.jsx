bf0a4d756ebc2d6057ffb023845c7b311c31ec88
pieter-rees <pieter@pieterrees.nl>
2017-11-26 00:14:35 +0100

    added react and a lot of base stuff

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React, { } from 'react';
+
+class Header extends React.Component {
+
+  render() {
+    return (
+      <div className="nice">
+        Test
+      </div>
+    );
+  }
+};
+
+export default Header;
