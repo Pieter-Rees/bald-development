b25e8510f38700474c8c7bc24f568364fe818641
pieter-rees <pieter@pieterrees.nl>
2017-11-28 22:09:59 +0100

    fixed first styled component :) also added stylelint-scss

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,24 @@
 import React, { } from 'react';
+import styled from 'styled-components';
+
+
+const Button = styled.button`
+  border-radius: 3px;
+  padding: 0.25em 1em;
+  margin: 0 1em;
+  background: transparent;
+  color: palevioletred;
+  border: 2px solid palevioletred;
+`;
 
 class Header extends React.Component {
 
   render() {
     return (
       <header className="header">
+        <Button>
+          TEST
+        </Button>
       </header>
     );
   }
