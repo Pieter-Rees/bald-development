cfb04ff7eecd8730899cf176dde2ab0ae44539fd
Pieter Rees <pieter@pieterrees.nl>
2018-08-14 17:23:24 +0200

    add aniamtion

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,5 +3,8 @@ import '../scss/app.scss';
 import preloader from './custom/preloader';
 import analytics from './custom/analytics';
 
+import customScroll from './custom/custom-scroll';
+
 preloader();
 analytics();
+customScroll();
